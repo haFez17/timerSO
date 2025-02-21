249c3dce8ca7d8766864e3c143aa9b3891dd7c52
haFez17 <rabbitcool515@gmail.com>
2025-02-21 15:41:19 +0200

    add main gui

--- pythonProject/Timer/TimerSO.py
+++ pythonProject/Timer/TimerSO.py
@@ -1,37 +1,5 @@
-from abc import ABC, abstractmethod
-import tkinter as tk
-from Function import TimerFunction
-
-class TimerInterface(ABC):
-    @abstractmethod
-    def start_timer(self):
-        pass
-
-    @abstractmethod
-    def stop_timer(self):
-        pass
-
-    @abstractmethod
-    def reset_timer(self):
-        pass
-
-class TimerApp(tk.Tk):
-    def __init__(self, timer_function: TimerInterface):
-        super().__init__()
-        self.timer_function = timer_function
-        self.title("Timer App")
-        self.geometry("300x200")
-
-        self.start_button = tk.Button(self, text="Start", command=self.timer_function.start_timer)
-        self.start_button.pack()
-
-        self.stop_button = tk.Button(self, text="Stop", command=self.timer_function.stop_timer)
-        self.stop_button.pack()
-
-        self.reset_button = tk.Button(self, text="Reset", command=self.timer_function.reset_timer)
-        self.reset_button.pack()
-
-if __name__ == "__main__":
-    timer_function = TimerFunction()
-    app = TimerApp(timer_function)
-    app.mainloop()
+import sys
+import time
+import threading
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QTabWidget, QMessageBox
+from PyQt6.QtCore import QTimer
