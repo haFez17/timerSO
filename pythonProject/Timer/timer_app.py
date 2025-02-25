b4b9bc237f37a7660f23f623d9569e6495818bcf
haFez17 <rabbitcool515@gmail.com>
2025-02-25 15:38:49 +0200

    test

--- pythonProject/Timer/timer_app.py
+++ pythonProject/Timer/timer_app.py
@@ -4,46 +4,43 @@ class TimerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Таймер и Будильник")
-        self.root.geometry("1280x720")
+        self.root.geometry("500x300")
         self.root.configure(bg="#DEB887")  # Цвет фона
 
-        # Создаем вкладки
+        # Вкладки
         self.tab_control = ttk.Notebook(root)
-
         self.timer_tab = ttk.Frame(self.tab_control)
         self.alarm_tab = ttk.Frame(self.tab_control)
-
         self.tab_control.add(self.timer_tab, text="Таймер")
         self.tab_control.add(self.alarm_tab, text="Будильник")
-
         self.tab_control.pack(expand=1, fill="both")
 
         # ====== Таймер ======
         self.timer_label = tk.Label(self.timer_tab, text="Оставшееся время: 00:00:00", font=("Courier", 14), fg="blue", bg="#F0FFFF")
-        self.timer_label.pack(pady=10)
+        self.timer_label.grid(row=0, column=0, columnspan=2, pady=10)
 
         self.timer_entry = tk.Entry(self.timer_tab, font=("Courier", 12), width=10, justify="center")
         self.timer_entry.insert(0, "00:00:10")
-        self.timer_entry.pack(pady=5)
+        self.timer_entry.grid(row=1, column=0, columnspan=2, pady=5)
 
         self.start_timer_btn = tk.Button(self.timer_tab, text="Старт Таймера", command=self.start_timer, font=("Times-Roman", 12), fg="green", bg="#444", width=20, height=2)
-        self.start_timer_btn.pack(pady=5)
+        self.start_timer_btn.grid(row=2, column=0, padx=10, pady=5)
 
         self.stop_timer_btn = tk.Button(self.timer_tab, text="Остановить Таймер", command=self.stop_timer, font=("Times-Roman", 12), fg="green", bg="#444", width=20, height=2)
-        self.stop_timer_btn.pack(pady=5)
+        self.stop_timer_btn.grid(row=2, column=1, padx=10, pady=5)
 
         self.timer_running = False
 
         # ====== Будильник ======
         self.alarm_label = tk.Label(self.alarm_tab, text="Будильник: Не установлен", font=("Courier", 14), fg="blue", bg="#F0FFFF")
-        self.alarm_label.pack(pady=10)
+        self.alarm_label.grid(row=0, column=0, columnspan=2, pady=10)
 
         self.alarm_entry = tk.Entry(self.alarm_tab, font=("Arial", 12), width=20, justify="center")
         self.alarm_entry.insert(0, datetime.now().strftime("%H:%M:%S"))  # Текущее время
-        self.alarm_entry.pack(pady=5)
+        self.alarm_entry.grid(row=1, column=0, columnspan=2, pady=5)
 
         self.set_alarm_btn = tk.Button(self.alarm_tab, text="Установить Будильник", command=self.set_alarm, font=("Arial", 12), fg="white", bg="#444", width=20, height=2)
-        self.set_alarm_btn.pack(pady=5)
+        self.set_alarm_btn.grid(row=2, column=0, columnspan=2, pady=5)
 
         self.alarm_running = False
 
